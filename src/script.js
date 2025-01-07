d27336c640a5210cc86b7352c8c706b1c5fe30b6
sarahrcalderon <srcalderonn@gmail.com>
2025-01-06 21:48:57 -0300

    tradutor

---
+++ src/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const text = "Design é resolver problemas com elegância e propósito";
+  const typingText = document.getElementById("typing-text");
+  typingText.style.color = "#ff0099";
+  let index = 0;
+  
+  function typeEffect() {
+    if (index < text.length) {
+      typingText.textContent += text[index];
+      index++;
+      setTimeout(typeEffect, 85); // Velocidade da digitação
+    } else {
+      addPontos(); 
+    }
+  }
+
+  
+
+  typeEffect();
+});
+
---
+++ src/tema.js
@@ -0,0 +1,29 @@
+const temaBtn = document.getElementById('theme-toggle');
+const temaIcone = document.getElementById('theme-icon');
+const currentTheme = localStorage.getItem('theme');
+
+// Se houver um tema salvo, aplica ele
+if (currentTheme === 'dark') {
+  document.body.classList.add('dark-theme');
+  temaIcone.classList.remove('bi-sun');
+  temaIcone.classList.add('bi-moon');
+} else {
+  document.body.classList.add('light-theme');
+  temaIcone.classList.remove('bi-moon');
+  temaIcone.classList.add('bi-sun');
+}
+temaBtn.addEventListener('click', () => {
+  if (document.body.classList.contains('light-theme')) {
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
+    temaIcone.classList.remove('bi-sun');
+    temaIcone.classList.add('bi-moon');
+    localStorage.setItem('theme', 'dark'); 
+  } else {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+    temaIcone.classList.remove('bi-moon');
+    temaIcone.classList.add('bi-sun');
+    localStorage.setItem('theme', 'light'); // Salva o tema no localStorage
+  }
+});
